dbd96f5b6bcab90f3930b3f915b332024d16c732
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2023-06-20 09:13:07 +0600

    ATM Machine Interface

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -1,2 +1,10 @@
-package atm;public class Main {
+package atm;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, SQLException {
+        Login login = new Login();
+        login.loginView();
+    }
 }
